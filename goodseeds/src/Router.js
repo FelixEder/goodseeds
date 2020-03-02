3ac4bab02f8e619601a6da73182881b2208c8462
Felix Eder <felix.eder@gmx.com>
2020-03-02 15:06:17 +0100

    Add router

---
+++ goodseeds/src/Router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StartPage from './components/StartPage.js'
+
+export default function Router() {
+  return (
+    <div className="router">
+      <StartPage/>
+    </div>
+  );
+}
