9b36e9e10819b4775547bd044f794f2c9370cc7f
August Jönsson <Augustjo@kth.se>
2020-03-19 16:17:12 +0100

    addplant reducer

---
+++ src/store/reducers/databaseReducer.js
@@ -0,0 +1,14 @@
+
+
+const addPlantReducer = (state, action) => {
+    switch (action.type){
+        case 'ADD_PLANT':
+            // add plant action dispatch update state.
+            
+        case 'ADD_PLANT_ERROR':
+            // signal error when adding plant. 
+
+        default:
+            return state;    
+    }
+}
