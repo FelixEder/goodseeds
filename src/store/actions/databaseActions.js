c2f3f2248ca2255f977a4935fa5a656bc6888200
August Jönsson <Augustjo@kth.se>
2020-03-19 12:02:08 +0100

    addplant action

---
+++ src/store/actions/databaseActions.js
@@ -0,0 +1,20 @@
+export const getPlants = (username) => {
+    
+}
+
+export const addPlant = (plant) => {
+    return (dispatch,getState,{getFirebase, getFirestore} ) => {
+        // make async call to database
+        // unsure what state is in this case
+        const firestore = getFirestore();
+        const state = getState();
+        firestore.collection('Users').doc(state.userID).update({
+            plants: firestore.FieldValue.arrayUnion(plant)
+        }).then(() => {
+            dispatch({type: 'ADD_PLANT', plant});
+        }).catch((err) => {
+            console.log('error: ' + err);
+            dispatch({type: 'ADD_PLANT_ERROR', err});
+        })
+    }
+}
