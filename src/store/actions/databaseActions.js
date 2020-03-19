4f9b8a5643379634487e0f71f8cb88c8922f218a
August Jönsson <Augustjo@kth.se>
2020-03-19 17:12:27 +0100

    very basic reducers that does not do much

--- src/store/actions/databaseActions.js
+++ src/store/actions/databaseActions.js
@@ -3,7 +3,7 @@ export const getPlants = (username) => {
 }
 
 export const addPlant = (plant) => {
-    return (dispatch,getState,{getFirebase, getFirestore} ) => {
+    return (dispatch,getState,{getFirebase, getFirestore}) => {
         // make async call to database
         // unsure what state is in this case
         const firestore = getFirestore();
@@ -15,6 +15,25 @@ export const addPlant = (plant) => {
         }).catch((err) => {
             console.log('error: ' + err);
             dispatch({type: 'ADD_PLANT_ERROR', err});
+        })
+    }
+}
+
+export const addReview = (reviewData) => {
+    return (dispatch,getState,{getFirebase, getFirestore}) => {
+        // make async call to database
+        const firestore = getFirestore();
+        const state = getState();
+        firestore.collection('Review').add({
+            plantID: reviewData.plantID,
+            username: reviewData.username,
+            rating: reviewData.rating,
+            timeStamp: reviewData.timeStamp,
+            reviewText: reviewData.reviewText,
+        }).then(() => {
+            dispatch({type: 'ADD_REVIEW', reviewData})
+        }).catch((err) => {
+            dispatch({type: 'ADD_REVIEW_ERROR', err})
         })
     }
 }
--- src/store/reducers/databaseReducer.js
+++ src/store/reducers/databaseReducer.js
@@ -4,11 +4,22 @@ const addPlantReducer = (state, action) => {
     switch (action.type){
         case 'ADD_PLANT':
             // add plant action dispatch update state.
-            
+
         case 'ADD_PLANT_ERROR':
             // signal error when adding plant. 
 
         default:
             return state;    
+    }
+}
+
+const addReviewReducer = (state,action) => {
+    switch (action.type){
+        case 'ADD_REVIEW':
+            // add review action update state?
+        case 'ADD_REVIEW_ERROR':
+            //signal error adding review
+        default:
+            return state;
     }
 }
