7166475773d71b11371ac14a8825a4ae85dadf5e
August Jönsson <Augustjo@kth.se>
2020-03-24 10:41:35 +0100

    function to update timesinceWaterd in db

--- src/store/actions/plantActions.js
+++ src/store/actions/plantActions.js
@@ -6,7 +6,7 @@ export const addPlant = (plant) => {
         const firestore = getFirestore();
         const state = getState();
         firestore.collection('Users').doc(state.userID).update({
-            plants: firestore.FieldValue.arrayUnion(plant)
+            Plants: firestore.FieldValue.arrayUnion(plant)
         }).then(() => {
             dispatch({type: 'ADD_PLANT', plant});
         }).catch((err) => {
@@ -15,3 +15,38 @@ export const addPlant = (plant) => {
         })
     }
 }
+
+export const waterPlant = (waterAction) => {
+    return (dispatch,getState,{getFirebase,getFirestore}) => {
+        const firestore = getFirestore();
+        const timeSinceWatered = waterAction.time;
+        const plantID = waterAction.plantID;
+        const state = getState();
+        var userDoc = firestore.collection('Users').doc(state.userID);
+        var plants = userDoc.get().then((doc) => {
+            if(doc.exists) {
+                return doc.Plants;
+            } else {
+                console.log('no such document exists');
+            }
+        })
+        plants = updateWateredTime(plants,waterAction)
+        firestore.collection('Users').doc(state.userID).set({
+            plants: plants
+        }).then(() => {
+            dispatch({type: 'WATERED_PLANT', })
+        }).catch((err) => {
+            dispatch({type:'WATERED_PLANT_ERROR', err })
+        })
+    }
+}
+
+
+const updateWateredTime = (plants,waterAction) => {
+  const plant = plants.find(plant => plant.plantID === waterAction.plantID);
+  const updatedPlant = {...plant ,timeSinceWatered: waterAction.time};
+  plants = plants.map(plant => {
+     return plant.plantID === waterAction.plantID? updatedPlant: plant 
+    })
+  return plants;  
+} 
