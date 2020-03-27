858a26f663da2a916edb13ad987ee33c61f0029b
Felix Eder <felix.eder@gmx.com>
2020-03-27 17:20:55 +0100

    Add function call to get trefle key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 import Router from './Router';
+const firebase = require("firebase");
 
 function App() {
+  var authTrefleApi = firebase.functions().httpsCallable('authTrefleApi');
+  authTrefleApi().then(function(result) {
+    console.log("Got result from backend");
+    console.log(result);
+  });
   return (
     <Router />
   );
