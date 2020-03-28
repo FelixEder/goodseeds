24b23783e37176e83bbfe515ab6def763d12cb59
Felix Eder <felix.eder@gmx.com>
2020-03-28 15:10:12 +0100

    Stringify the data before storing it to local storage

--- src/api/trefleAuth.js
+++ src/api/trefleAuth.js
@@ -3,7 +3,7 @@ const firebase = require("firebase");
 function getJWTToken() {
   var authTrefleApi = firebase.functions().httpsCallable('authTrefleApi');
   authTrefleApi().then(function(result) {
-    localStorage.setItem('token', result);
+    localStorage.setItem('token', JSON.stringify(result.data));
   });
 }
 
