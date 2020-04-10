2d6dba08bd868ec7e2434c53a448cb445d981247
August Jönsson <Augustjo@kth.se>
2020-04-10 14:43:29 +0200

    addreviewComponet

--- src/components/PlantDetails.js
+++ src/components/PlantDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import addReview from '../store/actions/reviewActions'
 
 const PlantDetails = ({reviews}) => {
   return(
@@ -33,6 +34,38 @@ const PlantDetails = ({reviews}) => {
 
     </div>
   );
+}
+
+const AddReviewComponent = () => {
+  return (
+    <div>
+      <form id='addReviewForm'>
+        <h1>Add your review on this plant!</h1>
+        <label>write your review here:</label><br/>
+        <textarea form='addReviewForm' id='reviewText'></textarea><br/>
+        <label for='rating'> rate the plant (between 1 and 5)</label>
+        <input id='rating' type='number' name='rating' min='1' max='5'></input>
+        <input type='submit' value='Add review' onClick={(event) => {
+          // send the complete review data to the action
+          let reviewForm = document.getElementById('addReviewForm');
+          const formData = new  formData(reviewForm);
+          let reviewdata = {
+            plantID: , 
+            username: ,
+            rating: formData.rating,
+            reviewText: formData.reviewText,
+            timestamp: event.timeStamp,
+          } 
+        }}/>
+      </form>
+    </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addReview: (reviewData) => dispatch(addReview(reviewData))
+  }
 }
 
 const mapStateToProps = (state) => {
