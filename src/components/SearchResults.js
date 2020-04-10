dbea467166f3d7012ceecfeffa04b55a984c7b69
Felix Eder <felix.eder@gmx.com>
2020-04-10 16:08:09 +0200

    Add way to search for images and display them

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import '../App.css';
 
 const SearchResults = ({searchResults}) => {
+
   return(
     <div className='search-results'>
       {searchResults.map(plant => (<span className='image-span'>
-        {plant.common_name ? (<span> <span> <h4> {plant.common_name} </h4> </span> <span> {plant.scientific_name} </span> </span>)
+        <img src={plant.imageURL} height='100' />
+        {plant.common_name ?
+          (<span> <span> <h4> {plant.common_name} </h4> </span> <span> {plant.scientific_name} </span> </span>)
         : (<span>{plant.scientific_name}</span>)}</span>))}
     </div>
   );
