41ae4e5b6c3ec0b012bbe3301018e97f15a5050c
larasm <larasm@kth.se>
2020-04-14 18:57:15 +0200

    Add connection to firebase database in userProfile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,28 +1,80 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { getPlantDetails } from '../api/trefleApiCalls';
 
-const UserProfile = ({auth}) => {
+// Takes in plantID, fetches information about plant and returns image and name
+// const fixPlantDetails = (plantID) => {
+//   getPlantDetails(plantID).then(plantDetails => {
+//           return (<span className='image-span'>
+//             <img src = {plantDetails.imageURL} height='100' />
+//             {plantDetails.common_name ?
+//               (<span>
+//                 <span>
+//                   <h4>
+//                     {plantDetails.common_name}
+//                   </h4>
+//                 </span>
+//                 <span>
+//                   {plantDetails.scientific_name}
+//                 </span>
+//               </span>)
+//             :
+//               (<span>
+//                 {plantDetails.scientific_name}
+//               </span>)
+//             }
+//           </span>)
+//         })
+// }
+
+const UserProfile = ({uid, user}) => {
+
   const history = useHistory();
 
-  if (!auth.uid) {
+  if (!uid) {
     history.push("/");
   }
 
-  return(
+
+
+  return (
     <div className='user-profile'>
-      This is the user profile
+      <h1>
+      Your plants
+      </h1>
+      <div>
+      {user ? user[0].plants.map(plant => {
+        return getPlantDetails(JSON.parse(plant).id).then(plantDetails => {
+          return (<span className='image-span'>
+             <img src={plantDetails.imageURL} height='100' />
+             {plantDetails.common_name ?
+             (<span> <span> <h4> {plantDetails.common_name} </h4> </span> <span> {plantDetails.scientific_name} </span> </span>)
+             : (<span>{plantDetails.scientific_name}</span>)}</span>
+          )
+        })
+        //return fixPlantDetails(JSON.parse(plant).id)
+      })
+      : "Loading..."}
+      </div>
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    uid: state.firebase.auth.uid,
+    user: state.firestore.ordered.Users
   }
 }
 
-export default connect(
-  mapStateToProps,
-  null
-)(UserProfile)
+export default compose (
+  connect(mapStateToProps, null),
+  firestoreConnect(props => {
+    if (!props.uid) return [];
+
+    return [{ collection: 'Users', doc: props.uid }];
+  }),
+)(UserProfile);
