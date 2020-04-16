904d8af9f82f874810b38dd43e67d45c1e66677f
Felix Eder <felix.eder@gmx.com>
2020-04-16 08:58:56 +0200

    Format PlantDetails

--- src/components/PlantDetails.js
+++ src/components/PlantDetails.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
-import { useParams } from 'react-router-dom';
-import { getPlantDetails } from '../api/trefleApiCalls';
+import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
+import {useParams} from 'react-router-dom';
+import {getPlantDetails} from '../api/trefleApiCalls';
 
 const PlantDetails = ({reviews}) => {
-  let { id } = useParams();
+  let {id} = useParams();
   console.log(id);
 
   const [plantDetails, setPlantDetails] = useState(null);
@@ -15,46 +15,49 @@ const PlantDetails = ({reviews}) => {
     setPlantDetails(details);
   })
 
-  return(
-    <div className='plant-details'>
-      <span className='plant-title'>
-        {plantDetails ?
-        plantDetails.scientific_name : null}
-        <h1>{id}</h1>
-      </span>
+  return (<div className='plant-details'>
+    <span className='plant-image'>
+      <img src={plantDetails
+          ? plantDetails.images[0]
+          : null} height='500'/> {
+        plantDetails
+          ? plantDetails.scientific_name
+          : null
+      }
+    </span>
 
-      {/* Reviews */}
-      { reviews ?
-              <div className='plant-reviews'>
-              {reviews.map(review => (
-                <div className='plant-review'>
-                  <span className='plant-review-rating'>
-                    Rating: {review.rating}
-                  </span>
+    {/* Reviews */}
+    {
+      reviews
+        ? <div className='plant-reviews'>
+            {
+              reviews.map(review => (<div className='plant-review'>
+                <span className='plant-review-rating'>
+                  Rating: {review.rating}
+                </span>
 
-                  <span className='plant-review-text'>
-                    Review text: {review.reviewText}
-                  </span>
+                <span className='plant-review-text'>
+                  Review text: {review.reviewText}
+                </span>
 
-                  <span className='plant-review-user'>
-                    {review.user}
-                  </span>
-                </div>
-              ))}
-            </div>
-            : null}
+                <span className='plant-review-user'>
+                  {review.user}
+                </span>
+              </div>))
+            }
+          </div>
+        : null
+    }
 
-    </div>
-  );
+  </div>);
 }
 
 const mapStateToProps = (state) => {
-  return {
-    reviews: state.firestore.ordered.Review
-  }
+  return {reviews: state.firestore.ordered.Review}
 }
 
-export default compose(
-  firestoreConnect(() => [{collection: 'Review'}]),
-  connect(mapStateToProps, null)
-)(PlantDetails)
+export default compose(firestoreConnect(() => [
+  {
+    collection: 'Review'
+  }
+]), connect(mapStateToProps, null))(PlantDetails)
