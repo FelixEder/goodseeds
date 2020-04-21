2ee5158be90d4548de94e98212da8c7542b53fd4
Felix Eder <felix.eder@gmx.com>
2020-04-21 09:48:20 +0200

    Update rootReducer to use the new plantAction and reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,6 +1,5 @@
-
-import authReducer from './authReducer'
-
+import plantReducer from './plantReducer';
+import authReducer from './authReducer';
 import searchResultsReducer from './searchResultsReducer';
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase'
@@ -9,6 +8,7 @@ import { firestoreReducer } from 'redux-firestore'
 const rootReducer = combineReducers({
     firestore: firestoreReducer,
     firebase: firebaseReducer,
+    plant: plantReducer,
     auth: authReducer,
     searchResults: searchResultsReducer
 })
