16c1564d7c84c9514944c2ee0b7998c4a64e7e9d
Felix Eder <felix.eder@gmx.com>
2020-04-23 15:06:52 +0200

    Update plant actions

--- src/store/actions/plantActions.js
+++ src/store/actions/plantActions.js
@@ -1,13 +1,22 @@
 
 export const addPlant = (action) => {
-    return (dispatch,getState,{getFirebase, getFirestore}) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         // make async call to database
         // unsure what state is in this case
         const firestore = getFirestore();
-        firestore.collection('Users').doc(action.userID).update({
-            Plants: firestore.FieldValue.arrayUnion(action.plant)
-        }).then(() => {
-            dispatch({type: 'ADD_PLANT', plant: action.plant});
+        const userDoc = firestore.collection('Users').doc(action.userID);
+        userDoc.get()
+        .then(doc => {
+          if (!doc.data().plants.some(plant => { return JSON.parse(plant).id === action.plantID; })) {
+            userDoc.update({
+              plants: firestore.FieldValue.arrayUnion(JSON.stringify({id: action.plantID, lastWatered: null, waterPeriod: null}))
+            });
+          } else {
+            throw "Plant already exists in garden";
+          }
+        })
+        .then(() => {
+            dispatch({type: 'ADD_PLANT', plant: action.plantID});
         }).catch((err) => {
             console.log('error: ' + err);
             dispatch({type: 'ADD_PLANT_ERROR', err});
