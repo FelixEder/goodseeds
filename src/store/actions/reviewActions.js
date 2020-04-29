50bc52dbeac49b30733d59b53501664fefe7f311
August Jönsson <Augustjo@kth.se>
2020-04-29 12:28:45 +0200

    addreview now adds new average rating. it works

--- src/store/actions/reviewActions.js
+++ src/store/actions/reviewActions.js
@@ -5,14 +5,34 @@ export const addReview = (reviewData, plantID) => {
         const firestore = getFirestore();
         const timeStamp = firestore.Timestamp.fromDate(new Date());
         reviewData.timeStamp = timeStamp;
-        const plantReview = firestore.collection('Review').doc(plantID);
-        plantReview.update({
-            Reviews: firestore.FieldValue.arrayUnion(JSON.stringify(reviewData))
-        }).then(() => {
-            dispatch({type: 'ADD_REVIEW', reviewData})
-        }).catch((err) => {
-            console.log(err);
-            dispatch({type: 'ADD_REVIEW_ERROR', err})
+        const plantReviewDocRef = firestore.collection('Plants').doc(plantID);
+        if (!plantReviewDocRef) {
+            // if the document does not exists create a new
+            plantReviewDocRef.set({
+                Reviews: [],
+                average_rating: 0
+            })
+        }
+        let newAverageRating = plantReviewDocRef.get().then((documentSnapShot) => {
+            let data = documentSnapShot.data();
+            let currentAverageRating = parseInt(data.average_rating);
+            let numbOfReviews = parseInt(data.Reviews.length);
+            numbOfReviews += 1;
+            // caluculate new average
+            newAverageRating = currentAverageRating + ((parseInt(reviewData.rating) - currentAverageRating) / numbOfReviews);
+            return newAverageRating;
+        }).then((rating) => {
+            plantReviewDocRef.update({
+                Reviews: firestore.FieldValue.arrayUnion(JSON.stringify(reviewData)),
+                average_rating: rating
+            }).then(() => {
+                dispatch({type: 'ADD_REVIEW', reviewData})
+            }).catch((err) => {
+                console.log(err);
+                dispatch({type: 'ADD_REVIEW_ERROR', err})
+            })
+
         })
+        
     }
 }
