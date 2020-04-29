ef621015ff6c0452046463ebf21178e7717aa22d
Felix Eder <felix.eder@gmx.com>
2020-04-29 09:39:27 +0200

    Revert "Update rootReducer to use the new plantAction and reducer"
    This reverts commit 2ee5158be90d4548de94e98212da8c7542b53fd4.

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,5 +1,6 @@
-import plantReducer from './plantReducer';
-import authReducer from './authReducer';
+
+import authReducer from './authReducer'
+
 import searchResultsReducer from './searchResultsReducer';
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase'
@@ -8,7 +9,6 @@ import { firestoreReducer } from 'redux-firestore'
 const rootReducer = combineReducers({
     firestore: firestoreReducer,
     firebase: firebaseReducer,
-    plant: plantReducer,
     auth: authReducer,
     searchResults: searchResultsReducer
 })
