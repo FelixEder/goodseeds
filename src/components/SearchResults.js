5e87b8b942923179eae539c16334f0c70b9229d4
Felix Eder <felix.eder@gmx.com>
2020-04-29 12:24:08 +0200

    WIP Move over search things to this file

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import '../App.css';
+import { useParams } from 'react-router-dom';
+import { searchPlants, getPlantDetails } from '../api/trefleApiCalls';
 
-const SearchResults = ({searchResults}) => {
+const SearchResults = () => {
+  let { searchString } = useParams();
+  let searchResults = "";
   const history = useHistory();
+
+  console.log(searchString);
+
+  searchPlants(searchString).then(results => {
+    const getImages = results.map(async (plant) => {
+      await getPlantDetails(plant.id).then(details => {
+        plant.imageURL = details.images.length > 0 ? details.images[0].url : null;
+      })
+    });
+
+    Promise.all(getImages)
+    .then((result) => console.log(result))
+  });
 
   return(
     <div className='search-results'>
@@ -16,7 +33,7 @@ const SearchResults = ({searchResults}) => {
           (<span> <span> <h4> {plant.common_name} </h4> </span> <span> {plant.scientific_name} </span> </span>)
         : (<span>{plant.scientific_name}</span>)}</span>))
         :
-        "No results found for search: <insert search>"}
+        "No results found for search \"" + searchString + "\""}
     </div>
   );
 }
