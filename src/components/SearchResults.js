10a19ff5fee2398120411e7fdc2a57083d20e235
Felix Eder <felix.eder@gmx.com>
2020-04-29 11:42:06 +0200

    Check for length of SearchResults and display an error message if none was found

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -8,11 +8,15 @@ const SearchResults = ({searchResults}) => {
 
   return(
     <div className='search-results'>
-      {searchResults.map(plant => (<span className='image-span' onClick={() => { history.push("/plantDetails/" + plant.id) }}>
+      {searchResults.length > 0
+        ?
+        searchResults.map(plant => (<span className='image-span' onClick={() => { history.push("/plantDetails/" + plant.id) }}>
         <img src={plant.imageURL} height='100' />
         {plant.common_name ?
           (<span> <span> <h4> {plant.common_name} </h4> </span> <span> {plant.scientific_name} </span> </span>)
-        : (<span>{plant.scientific_name}</span>)}</span>))}
+        : (<span>{plant.scientific_name}</span>)}</span>))
+        :
+        "No results found for search: <insert search>"}
     </div>
   );
 }
