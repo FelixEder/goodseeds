77aa51cec8bd0beea1e496d8904e5be47c604f85
larasm <larasm@kth.se>
2020-05-01 11:00:14 +0200

    Add nice searchbar but it's not so nice really needs to be changed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { searchPlants, getPlantDetails } from '../api/trefleApiCalls';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const SearchBar = ({updateResults}) => {
-  let searchInput;
   const history = useHistory();
+  const [searchstring, setSearchstring] = React.useState("")
 
   function handleSubmit(event) {
     event.preventDefault();
-    searchPlants(searchInput.value).then(results => {
+    searchPlants(searchstring).then(results => {
 
       const getImages = results.map(async (plant) => {
         await getPlantDetails(plant.id).then(details => {
@@ -26,8 +28,8 @@ const SearchBar = ({updateResults}) => {
   return (
     <div className='search-bar'>
       <form onSubmit={handleSubmit}>
-        <input placeholder='Type to search' ref={node => searchInput = node} />
-        <button type="submit">Search</button>
+        <TextField id="filled" label="Type to search" variant="outlined" onChange={(event) => setSearchstring(event.target.value)}/>
+        <Button type="submit" variant="filled" onClick={handleSubmit}>Search</Button>
       </form>
     </div>
   )
