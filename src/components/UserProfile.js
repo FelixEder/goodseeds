31afec7d347228f50171a305ab2f341cbf81fc4e
larasm <larasm@kth.se>
2020-05-04 18:03:22 +0200

    User profile now uses materials

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -6,8 +6,82 @@ import { compose } from 'redux';
 import { getPlantDetails } from '../api/trefleApiCalls';
 import RenderPromise from '../util/renderPromise'
 import { waterPlant, updateWaterPeriod } from '../store/actions/plantActions';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  cardPlantNeedsWater: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    border : '2px red solid'
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardNeedsWater: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    border: '2px red solid'
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 const UserProfile = ({uid, user, waterPlant, updateWaterPeriod}) => {
+
+  const handleChange = (event, plantID) => {
+    if (event) {
+      updateWaterPeriod({userID: uid, plantID, waterPeriod: event.target.value})
+    }
+
+  };
+
   function treatAsUTC(date) {
     var result = new Date(date);
     result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
@@ -20,65 +94,66 @@ const UserProfile = ({uid, user, waterPlant, updateWaterPeriod}) => {
   }
 
   const history = useHistory();
+  const classes = useStyles();
 
   // Takes in plantID, fetches information about plant and returns image and name
   const createPlantDisplay = (genericPlant, userPlant) => {
-    let waterPeriodInput;
-    let border = {};
-    
-    if (daysBetween(new Date(userPlant.lastWatered), new Date()) >= userPlant.waterPeriod) {
-      border = {border: '3px red solid'};
-    }
-    
-    return (<span className='image-span' style={border}>
-    <img src={(genericPlant.images && genericPlant.images.length > 0) ? genericPlant.images[0].url : null} height='100' onClick={() => {history.push("/plantDetails/" + genericPlant.id)}}/>
-    {genericPlant.common_name ?
-      (<span>
-        <span>
-          <br/>
-          <b>
-            {genericPlant.common_name}
-          </b>
-        </span>
-        <span>
-          <br/>
-          <i>{genericPlant.scientific_name}</i>
-        </span>
-      </span>)
-        :
-        (<span>
-          {genericPlant.scientific_name}
-          </span>)
-        }
-        <div>
-          Last watered {daysBetween(new Date(userPlant.lastWatered), new Date())} days ago
-        </div>
-        {userPlant.waterPeriod 
+
+
+    return (<Card className={(daysBetween(new Date(userPlant.lastWatered), new Date()) >= userPlant.waterPeriod) ? classes.cardPlantNeedsWater : classes.card}>
+    <CardMedia
+      className={classes.cardMedia}
+      image={(genericPlant.images && genericPlant.images.length > 0) ? genericPlant.images[0].url : null}
+      title="Image title"
+      onClick={() => {history.push("/plantDetails/" + genericPlant.id)}}
+    />
+    <CardContent className={classes.cardContent}>
+
+      <Typography gutterBottom variant="h5" component="h2">
+      {genericPlant.common_name}
+      </Typography>
+
+      <Typography>
+      Last watered {daysBetween(new Date(userPlant.lastWatered), new Date())} days ago
+      </Typography>
+
+      
+      {userPlant.waterPeriod 
           ? 
-          (<div>
-            Needs to be watered every {userPlant.waterPeriod} days
-          </div>)
+          <Typography><i>Needs to be watered every {userPlant.waterPeriod} days</i></Typography>
           :
-          (<div className='blinking'>
-            You need to set how often this plant needs to be watered below
-          </div>)
-        }
-        <div>
-          <button onClick={(() => waterPlant({userID: uid, plantID: userPlant.id}))}>water</button>
-        </div>
-        <div>
-          Change how often this plant needs to be watered:
-          <form onSubmit={event => { 
-            event.preventDefault(); 
-            if (waterPeriodInput.value > 0) { 
-              updateWaterPeriod({userID: uid, plantID: userPlant.id, waterPeriod: waterPeriodInput.value}) 
-            }
-          }}>
-            <input placeholder={userPlant.waterPeriod} type='number' ref={node => waterPeriodInput = node} style={{ width: '90%' }} />
-            <button>Update</button>
-          </form>
-        </div>
-        </span>)
+          <Typography><i className='blinking'>You need to set how often this plant needs to be watered below</i></Typography>
+      }
+      
+    </CardContent>
+    <CardActions>
+      <Button size="small" color="primary" onClick={(() => waterPlant({userID: uid, plantID: userPlant.id}))}>
+        Water
+      </Button>
+        <FormControl className={classes.formControl}>
+        <InputLabel>Water period</InputLabel>
+        <Select
+          value={userPlant.waterPeriod ? userPlant.waterPeriod : 0}
+          onChange={(event) => handleChange(event, userPlant.id)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+        </Select>
+      </FormControl>
+    </CardActions>
+  </Card>)
+
   }
 
   // If no user is logged in, return to start page
@@ -87,28 +162,45 @@ const UserProfile = ({uid, user, waterPlant, updateWaterPeriod}) => {
   }
 
   return (
-    <div className='user-profile'>
-      <h1>
-      Your garden
-      </h1>
-      {
-        // If user has loaded, retrieve all plants and create plant display for each
-        user ? (
-          <div>
-          {user[0].plants.map(plant => {
-            return (
-                <span key={plant}>
-                  {
-                    // Call RenderPromise, and then render the data
-                    <RenderPromise promise={getPlantDetails(JSON.parse(plant).id)} renderData={({data}) => (<span>{createPlantDisplay(data, JSON.parse(plant))}</span>)}/>
-                  }
-                </span>)
-          })}
-          </div>
-        ) : (<div>Loading...</div>)
-      }
-    </div>
-  )
+    <React.Fragment>
+      <CssBaseline />
+
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Your garden
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Water your plants below!
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            { user ? 
+            user[0].plants.map((plant) => (
+              <RenderPromise promise={getPlantDetails(JSON.parse(plant).id)} renderData={({data}) => (<Grid item key={plant} xs={12} sm={6} md={4}>{createPlantDisplay(data, JSON.parse(plant))}</Grid>)}/>
+            ))
+            : null }
+            
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
 
 }
 
