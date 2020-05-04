450a86db14254a445c71e39e5b15917e79f513d4
larasm <larasm@kth.se>
2020-05-04 14:30:22 +0200

    Fix so Top rated plant and random review is shown on start page.

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -6,25 +6,40 @@ import { getPlantDetails } from '../api/trefleApiCalls';
 import RenderPromise from '../util/renderPromise'
 import { useHistory } from 'react-router-dom';
 import daysBetween from '../util/dateHandler.js'
+import logo from '../logo.png';
 
-const StartPage = ({reviews, uid, users}) => {
+const StartPage = ({plants, uid, users}) => {
   const history = useHistory();
 
-  const numPlantsNeedWatering = (users) => {
-    if (!users) return null;
 
+  const numPlantsNeedWatering = (users) => {
+    if (!users || !uid) return null;
     return (users
           .filter(user => user.id == uid)[0].plants // Access users plants
-          .filter(plant => (daysBetween(new Date(JSON.parse(plant).lastWatered), new Date()) > JSON.parse(plant).waterPeriod)).length)
+          .filter(plant => (daysBetween(new Date(JSON.parse(plant).lastWatered), new Date()) > JSON.parse(plant).waterPeriod)))
+  }
+
+  const sortAvgRating = (a, b) => {
+    if (a.avg_rating > b.avg_rating) {
+      return -1
+    } else if (b.avg_rating < a.avg_rating) {
+      return 1
+    }
+    return 0
+  }
+
+  const generateRandomPlantandReview = () => {
+    var potentialPlants = plants.filter(plant => (plant.reviews !== undefined && plant.reviews.length >= 0))
+    var randomPlant = potentialPlants[Math.floor(Math.random()*potentialPlants.length)];
+    var randomReview = JSON.parse(randomPlant.reviews[[Math.floor(Math.random()*randomPlant.reviews.length)]])
+    return <RenderPromise promise={getPlantDetails(randomPlant.id)} renderData={({data}) => {return createReviewDisplay(data, randomReview)}}/>
+    
   }
 
   const createReviewDisplay = (plantDetails, reviewDetails) => {
     return(
       <span className='image-span'>
-        {plantDetails.images !== undefined && plantDetails.images.length !== 0 ?
-          (<img src={plantDetails.images[0].url} alt="review of plant" height='100' onClick={() => {history.push("/plantDetails/" + plantDetails.id)}}/>)
-          : null
-        }
+        <img src={(plantDetails.images && plantDetails.images.length > 0) ? plantDetails.images[0].url : logo} alt="review of plant" height='100' onClick={() => {history.push("/plantDetails/" + plantDetails.id)}}/>
         
         <h2>
           {plantDetails.common_name ? plantDetails.common_name : plantDetails.scientific_name}
@@ -45,21 +60,14 @@ const StartPage = ({reviews, uid, users}) => {
   const createTopPlantDisplay = (plantDetails, reviewDetails) => {
     return(
       <span className='image-span'>
-        {plantDetails.images !== undefined && plantDetails.images.length !== 0 ?
-          (<img src={plantDetails.images[0].url} alt="review of plant" height='100' onClick={() => {history.push("/plantDetails/" + plantDetails.id)}}/>)
-          : null
-        }
+        <img src={(plantDetails.images && plantDetails.images.length > 0) ? plantDetails.images[0].url : logo} alt="review of plant" height='100' onClick={() => {history.push("/plantDetails/" + plantDetails.id)}}/>
         
         <h2>
           {plantDetails.common_name ? plantDetails.common_name : plantDetails.scientific_name}
         </h2>
 
         <p>
-          Rating: {reviewDetails.rating}
-        </p>
-
-        <p>
-          <b>User: </b>{reviewDetails.username}
+          Average Rating: {reviewDetails.avg_rating.toFixed(2)}
         </p>
 
       </span>
@@ -77,38 +85,37 @@ const StartPage = ({reviews, uid, users}) => {
         <h2>
         Top rated plants
         </h2>
-        {reviews ? reviews.map(review => {
-            if (review.plantID) {
-              return (
-                <RenderPromise promise={getPlantDetails(review.plantID)} renderData={({data}) => {return createTopPlantDisplay(data, review)}}/>
-              )
-            } else {
-              return null
-            }           
-          })
-          : null}
+
+        {
+          plants ?
+            plants.slice()             // Copy array
+                  .sort(sortAvgRating) // Sort according to avg rating
+                  .slice(0,5)          // Take first 5 elements
+                  .map(plant => {
+                    return (
+                      <RenderPromise promise={getPlantDetails(plant.id)} renderData={({data}) => {return createTopPlantDisplay(data, plant)}}/>
+                    )
+                  })
+          : null
+        }
       </div>
 
       <div>
         <h2>
-        Recently reviewed
+        Random review
         </h2>
-          {reviews ? reviews.map(review => {
-            if (review.plantID) {
-              return (
-                <RenderPromise promise={getPlantDetails(review.plantID)} renderData={({data}) => {return createReviewDisplay(data, review)}}/>
-              )
-            } else {
-              return null
-            }
-          })
-          : null}
+
+          {
+            plants ?
+            generateRandomPlantandReview()
+            : null
+          }
       </div>
 
       <h2>
         Your garden
       </h2>
-        {users === undefined
+        {!users || !uid
           ? (<p>Log in or sign up to see your garden</p>)
           : (<p>Your garden has {numPlantsNeedWatering(users)} plants in need of watering!</p> ) 
         }
@@ -119,7 +126,7 @@ const StartPage = ({reviews, uid, users}) => {
 
 const mapStateToProps = (state) => {
   return {
-    reviews: state.firestore.ordered.Review,
+    plants: state.firestore.ordered.Plants,
     uid: state.firebase.auth.uid,
     users: state.firestore.ordered.Users
   }
@@ -128,7 +135,7 @@ const mapStateToProps = (state) => {
 export default compose (
 
   firestoreConnect((props) => {
-    return [{ collection: 'Review' }, { collection: 'Users', doc: props.uid }];
+    return [{ collection: 'Plants' }, { collection: 'Users', doc: props.uid }];
   }),
   
   connect(mapStateToProps, null),
