769c98449062af55a80f35bff59e9006c8be9c14
larasm <larasm@kth.se>
2020-05-05 10:33:46 +0200

    Remove comments

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -109,16 +109,6 @@ const SearchResults = ({searchResults}) => {
     </React.Fragment>
   );
 
-  // return(
-  //   <div className='search-results'>
-  //     {searchResults.map(plant => (<span className='image-span' onClick={() => { history.push("/plantDetails/" + plant.id) }}>
-  //       <img src={plant.imageURL} height='100' />
-  //       {plant.common_name ?
-  //         (<span> <span> <h4> {plant.common_name} </h4> </span> <span> {plant.scientific_name} </span> </span>)
-  //       : (<span>{plant.scientific_name}</span>)}</span>))}
-  //   </div>
-  // );
-
 }
 
 export default connect(
