5158e195fa0042a70563548da1d0d27ab6705012
larasm <larasm@kth.se>
2020-05-07 14:10:02 +0200

    Add logo, explain complete data more

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import logo from '../logo.png';
 
 
 const About = () => {
@@ -20,15 +21,21 @@ const About = () => {
                             Welcome to Goodseeds!
                         </Typography>
                         <Typography variant="h5" component="h2" gutterBottom>
-                            This page is for searching, reviewing and keeping track of your plants.
+                            This is a tool for searching, reviewing and keeping track of your plants.
                         </Typography>
                         <Typography variant="h6" component="h2" gutterBottom>
-                            Start by signing up or logging in, then you can start searching for plants in the bar above! The box <i>Complete data</i> allows you to search for plants that have complete data.
+                            Start by signing up or logging in, then you can start searching for plants in the bar above! 
                         </Typography>
                         <Typography variant="h6" component="h2" gutterBottom>
                             When you search for a plant, you can add it to your garden. Update its water period and remember to water it as often as you want!
                         </Typography>
-
+                        <Typography variant="caption">
+                        Some of the plants in the search results may not have images, have information on fire resistance etc. The box <i>Complete data</i> allows you to search for plants that have complete data, meaning all fields are filled in.
+                        </Typography>
+                        <img src={logo} alt={"Goodseeds"}/>
+                        <br/>
+                        <br/>
+                        <br/>
             </Container>
             <footer gutterBottom>
                 <Typography variant="h6" align="center" gutterBottom>
