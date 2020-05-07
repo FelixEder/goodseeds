a3af93b90e9459924dd9ca0554bea0415c42169c
larasm <larasm@kth.se>
2020-05-07 14:03:34 +0200

    Fix about page, now displays information about how to use the website

--- src/components/About.js
+++ src/components/About.js
@@ -14,41 +14,30 @@ const About = () => {
     <div>
         <CssBaseline />
             <Container maxWidth="sm">
-                <Card>
-                    <CardContent>
+                {/* <Card>
+                    <CardContent> */}
                         <Typography variant="h2" component="h1" gutterBottom>
                             Welcome to Goodseeds!
                         </Typography>
                         <Typography variant="h5" component="h2" gutterBottom>
                             This page is for searching, reviewing and keeping track of your plants.
                         </Typography>
-                        {/* <Typography variant="caption" component="h2" gutterBottom>
-                            Sign up or log in
+                        <Typography variant="h6" component="h2" gutterBottom>
+                            Start by signing up or logging in, then you can start searching for plants in the bar above! The box <i>Complete data</i> allows you to search for plants that have complete data.
                         </Typography>
-                        <Typography variant="caption" component="h2" gutterBottom>
-                            Search for plants to add to your garden
+                        <Typography variant="h6" component="h2" gutterBottom>
+                            When you search for a plant, you can add it to your garden. Update its water period and remember to water it as often as you want!
                         </Typography>
-                        <Typography variant="caption" component="h2" gutterBottom>
-                            Water your plants
-                        </Typography>
-                        <Typography variant="caption" component="h2" gutterBottom>
-                            Review your plants
-                        </Typography> */}
 
-                    </CardContent>
-                    <CardActions justify='center'>
-                        <Button>
-                            Sign up
-                        </Button>
-                        <Button>
-                            Log in
-                        </Button>
-                        <Button>
-                            Go to Garden
-                        </Button>
-                    </CardActions>
-                </Card>
             </Container>
+            <footer gutterBottom>
+                <Typography variant="h6" align="center" gutterBottom>
+                    This website was created for [DD2642]
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                    Created by Anton, August, Felix and Lara
+                </Typography>
+            </footer>
     </div>
   );
 }
