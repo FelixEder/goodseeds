4f3f3489bd2c0d6f4f2704315484101558f005e1
larasm <larasm@kth.se>
2020-05-07 18:05:34 +0200

    Remove unused import

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import logo from '../logo.png';
 
 
