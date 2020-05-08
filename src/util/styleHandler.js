abe1d478e2c3fcd3c0dfeb1fe96a90767e279c3f
larasm <larasm@kth.se>
2020-05-08 11:39:50 +0200

    Add styling to sign up/sign in

--- src/util/styleHandler.js
+++ src/util/styleHandler.js
@@ -51,6 +51,23 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: theme.palette.background.paper,
       padding: theme.spacing(6),
     },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
 }));
 
 export default useStyles
